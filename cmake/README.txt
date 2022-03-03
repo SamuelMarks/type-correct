92ddfcdc64179cf1fbc1794ba9d2495aedfdb886
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-03-03 18:20:57 -0500

    [cmake/modules/AcquireGoogleTest.cmake] Update gtest version; [cmake/*] Add boilerplate for cpack &etc; [type_correct/CMakeLists.txt] Add boilerplate for CPack; [type_correct/type_correct/CMakeLists.txt] Use GNU install targets (should also fix MSVC?); [type_correct/type_correct/tests/CMakeLists.txt] Try `gtest_discover_tests` to see if `add_test` explicitly was causing the build error on macOS

---
+++ cmake/README.txt
@@ -0,0 +1,7 @@
+type-correct
+
+LLVM LibClang / LibTooling solution to 'fix' types, rewriting inconsistent use of types to make them consistent.
+
+------------------------------------------------------------------------
+
+Licensed under CC0
--- cmake/modules/AcquireGoogleTest.cmake
+++ cmake/modules/AcquireGoogleTest.cmake
@@ -3,7 +3,7 @@ macro (acquire_google_test)
     FetchContent_Declare(
             googletest
             # Specify the commit you depend on and update it regularly.
-            URL https://github.com/google/googletest/archive/609281088cfefc76f9d0ce82e1ff6c30cc3591e5.zip
+            URL https://github.com/google/googletest/archive/c9461a9b55ba954df0489bab6420eb297bed846b.zip
     )
     # For Windows: Prevent overriding the parent project's compiler/linker settings
     set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
